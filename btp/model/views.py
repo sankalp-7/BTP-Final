ddccf79276edce7c535083f5ff9d3bfabbbdd62c
sankalp <sankalp@sankalps-mbp.in.ibm.com>
2024-04-02 12:32:09 +0530

    backend done,deploying left

--- btp/model/views.py
+++ btp/model/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-
+import csv
+import requests
 # Create your views here.
 
 
 def home(request):
-    return render(request,'model/home.html')
+    if request.method == 'POST' and request.FILES.get('csvfile'):
+        input_file = request.FILES['csvfile']
+        data_rows = []
+        
+        # Read the uploaded CSV file
+        csv_content = input_file.read().decode('utf-8-sig')  # Remove BOM
+        csv_reader = csv.reader(csv_content.splitlines())
+        
+        for row in csv_reader:
+            row_string = ','.join(row)
+            data_rows.append(row_string)
+        
+        print(data_rows)
+        url = "" #to be added after deploying
+        payload="\n".join(data_rows)
+
+        headers = {
+        'Content-Type': 'text/csv'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        return render(request, 'model/home.html', {'res': response.text})
+    
+    return render(request, 'model/home.html')
